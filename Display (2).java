201fb707391a3d7d3b7210be8fb88bfd146e69a3
KoustabhBiswas <biswas.koustabh@gmail.com>
2017-06-24 14:42:33 +0530

    ProtectedExample

---
+++ Display (2).java
@@ -0,0 +1,11 @@
+package Protectedexample;
+import ProtectedDisplay.*;
+public class Display  extends  class1{
+         
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Display obj1 = new Display();
+    obj1.disp();
+	}
+
+}
---
+++ class1.java
@@ -0,0 +1,7 @@
+package ProtectedDisplay;
+
+public class class1 {
+   protected void disp(){
+	 System.out.println("You are a user");
+ }
+}
